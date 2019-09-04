bb9948e289d8bd379c741f7a5a47ec6fcb040acb
nazimkazim <nazik_111@mail.ru>
2019-09-04 19:32:42 +0600

    add context and format data

--- src/components/Services.js
+++ src/components/Services.js
@@ -10,22 +10,22 @@ export default class Services extends Component {
             {
                 icon:<FaCocktail/>,
                 title:'free coctails',
-                info:'Dolor irure id exercitation et irure voluptate aliquip quis. Consectetur commodo est reprehenderit deserunt nisi quis et. Amet non do elit commodo veniam amet veniam exercitation do dolore. In consequat nostrud consequat dolor et qui est ipsum. Excepteur ad et consequat tempor deserunt exercitation ipsum occaecat laboris Lorem dolore pariatur.' 
+                info:'Dolor irure id exercitation et irure voluptate aliquip quis.' 
             },
             {
                 icon:<FaHiking/>,
                 title:'endless hiking',
-                info:'Duis cillum laboris quis mollit adipisicing pariatur cupidatat et. Esse veniam irure id ut et tempor ullamco laborum. In do magna quis sint commodo sunt dolor tempor nostrud do. Tempor nostrud adipisicing sint est aliqua. Tempor aliquip proident eiusmod laboris irure.' 
+                info:'Duis cillum laboris quis mollit adipisicing pariatur cupidatat et.' 
             },
             {
                 icon:<FaShuttleVan/>,
                 title:'free shuttle',
-                info:'Velit nulla nulla ea aliqua velit minim nulla est adipisicing adipisicing amet aliqua. Reprehenderit laboris ea et qui. Occaecat do pariatur nulla adipisicing proident sunt proident non cillum id veniam. Eiusmod est ut qui et quis ea culpa magna velit minim voluptate. Non ipsum eiusmod eu culpa laborum ad id laborum dolore. Labore reprehenderit proident excepteur aute officia occaecat velit. Aliqua non fugiat excepteur laborum amet anim.' 
+                info:'Velit nulla nulla ea aliqua velit minim nulla est adipisicing adipisicing amet aliqua.' 
             },
             {
                 icon:<FaBeer/>,
                 title:'strongest beer',
-                info:'Dolor occaecat quis in culpa ut adipisicing qui est. Cillum esse qui consectetur labore nisi. Ipsum culpa reprehenderit exercitation aliquip sit proident reprehenderit commodo dolor.' 
+                info:'Dolor occaecat quis in culpa ut adipisicing qui est.' 
             }
         ]
     }
