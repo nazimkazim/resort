81082f7bf6d5eca9b3635de0f324ae969964a6c2
nazimkazim <nazik_111@mail.ru>
2019-09-06 16:53:39 +0600

    add room list

---
+++ src/components/RoomsFilter.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const RoomsFilter = () => {
+    return (
+        <div>
+            Hello from room filter
+        </div>
+    )
+}
+
+export default  RoomsFilter
--- src/context.js
+++ src/context.js
@@ -12,14 +12,27 @@ class RoomProvider extends Component {
         rooms:[],
         sortedRooms:[],
         featuredRooms:[],
-        loading:true
+        loading:true,
+        type:'all',
+        capacity:1,
+        price:0,
+        minPrice:0,
+        maxPrice:0,
+        minSize:0,
+        maxSize:0,
+        breakfast:false,
+        pets:false
     }
 
     componentDidMount() {
         let rooms = this.formatData(items);
         let featuredRooms = rooms.filter(room => room.featured === true);
+
+        let maxPrice = Math.max(...rooms.map(item => item.price))
+        let maxSize = Math.max(...rooms.map(item => item.size))
+        
         this.setState({
-            rooms, featuredRooms, sortedRooms:rooms, loading:false
+            rooms, featuredRooms, sortedRooms:rooms, loading:false, maxPrice, maxSize
         })
         console.log(rooms);
     }
@@ -46,5 +59,12 @@ class RoomProvider extends Component {
 }
 
 const RoomConsumer = RoomContext.Consumer;
+
+export function withRoomConsumer(Component) {
+    return function ConsumerWrapper(props) {
+        return <RoomConsumer>{value => <Component {...props} context={value}/>}</RoomConsumer>
+
+    }
+}
 
 export {RoomProvider, RoomConsumer, RoomContext} 
