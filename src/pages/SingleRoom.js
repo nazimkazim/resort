b8068a22647488f09090cc3193869ff249c9ef77
nazimkazim <nazik_111@mail.ru>
2019-09-06 11:27:38 +0600

    add complete single room page

--- src/pages/SingleRoom.js
+++ src/pages/SingleRoom.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react'
 import defaultBcg from '../images/room-1.jpeg'
-import Hero from '../components/Hero';
 import Banner from '../components/Banner';
 import {Link} from 'react-router-dom';
 import {RoomContext} from '../context';
+import StyledHero from '../components/StyledHero'
 
 
 export default class SingleRoom extends Component {
@@ -31,11 +31,49 @@ export default class SingleRoom extends Component {
                 </div>
             )
         } 
-        console.log(room)
+        
+        const {name, description,capacity,size, price, extras, breakfast, pets, images} = room
         return (
-            <div>
-                Hello from single room page
-            </div>
+            <>
+                <StyledHero img={images[0] || this.state.defaultBcg}>
+                    <Banner title={`${name} room`}>
+                        <Link to="/rooms" className="btn-primary">back to rooms</Link>    
+                    </Banner>    
+                </StyledHero>
+                <section className="single-room">
+                    <div className="single-room-images">
+                        {images.map((item, index) =>{
+                            return <img key={index} src={item} alt="" />
+                        })}
+                    </div>
+                    <div className="single-room-info">
+                        <article className="info">
+                            <h3>Details</h3>
+                            <p>{description}</p>
+                        </article>
+                        <article className="info">
+                            <h3>Info</h3>
+                            <h6>price: ${price}</h6>
+                            <h6>size: ${size} SQFT</h6>
+                            <h6>
+                                max capacity : {
+                                    capacity > 1 ? `${capacity} people` : `${capacity} person`
+                                }
+                            </h6>
+                            <h6>pets: {pets ? "allowed" : "not allowed"}</h6>
+                            <h6>{breakfast && "free breakfast included"}</h6>
+                        </article>
+                    </div>
+                </section>
+                <section className="room-extras">
+                <h6>Extras</h6>
+                <ul className="extras">
+                  {extras.map((item, index) => {
+                    return <li key={index}>- {item}</li>
+                  })}
+                </ul>                
+                </section>
+            </>
         )
     }
 }
