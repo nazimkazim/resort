1b973d26e6c5014e4fb16282b4a5a5398a1a003a
nazimkazim <nazik_111@mail.ru>
2019-09-13 11:28:09 +0600

    add fix bug with extras field

---
+++ src/Contentful.js
@@ -0,0 +1,6 @@
+import {createClient} from 'contentful'
+
+export default createClient({
+    space:'i3c5ztysw1sc',
+    accessToken:'Eyp9bPVkzDnTgTiEFukDZVqHuEOL6NsZ4ZtJn_5wCtw'
+})
--- src/context.js
+++ src/context.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import items from './data';
+import Client from './Contentful'
 
 
 
@@ -8,98 +9,139 @@ const RoomContext = React.createContext();
 
 
 class RoomProvider extends Component {
-    state={
-        rooms:[],
-        sortedRooms:[],
-        featuredRooms:[],
-        loading:true,
-        type:'all',
-        capacity:1,
-        price:0,
-        minPrice:0,
-        maxSize:0,
-        maxPrice:0,
-        minSize:0,
-        breakfast:false,
-        pets:false
+  state = {
+    rooms: [],
+    sortedRooms: [],
+    featuredRooms: [],
+    loading: true,
+    type: "all",
+    capacity: 1,
+    price: 0,
+    minPrice: 0,
+    maxSize: 0,
+    maxPrice: 0,
+    minSize: 0,
+    breakfast: false,
+    pets: false
+  };
+
+  getData = async () => {
+    try {
+      let response = await Client.getEntries({
+        content_type: "beachResortProject"
+      });
+
+      let rooms = this.formatData(response.items);
+      let featuredRooms = rooms.filter(room => room.featured === true);
+
+      let maxPrice = Math.max(...rooms.map(item => item.price));
+      let maxSize = Math.max(...rooms.map(item => item.size));
+
+      this.setState({
+        rooms,
+        featuredRooms,
+        sortedRooms: rooms,
+        loading: false,
+        maxPrice,
+        maxSize
+      });
+      console.log(rooms);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  componentDidMount() {
+    this.getData();
+  }
+
+  formatData(items) {
+    let tempItems = items.map(item => {
+      let id = item.sys.id;
+      let images = item.fields.images.map(image => image.fields.file.url);
+      let room = { ...item.fields, images, id };
+      return room;
+    });
+    return tempItems;
+  }
+
+  getRoom = slug => {
+    let tempRooms = [...this.state.rooms];
+    const room = tempRooms.find(room => room.slug === slug);
+    return room;
+  };
+
+  handleChange = event => {
+    const target = event.target;
+    const value = target.type === "checkbox" ? target.checked : target.value;
+    const name = event.target.name;
+    this.setState(
+      {
+        [name]: value
+      },
+      this.filterRooms
+    );
+  };
+
+  filterRooms = () => {
+    let {
+      rooms,
+      type,
+      capacity,
+      price,
+      minSize,
+      minPrice,
+      maxPrice,
+      maxSize,
+      breakfast,
+      pets
+    } = this.state;
+
+    let tempRooms = [...rooms];
+
+    capacity = parseInt(capacity);
+    price = parseInt(price);
+
+    if (type !== "all") {
+      tempRooms = tempRooms.filter(room => room.type === type);
     }
 
-    componentDidMount() {
-        let rooms = this.formatData(items);
-        let featuredRooms = rooms.filter(room => room.featured === true);
-
-        let maxPrice = Math.max(...rooms.map(item => item.price))
-        let maxSize = Math.max(...rooms.map(item => item.size))
-        
-        this.setState({
-            rooms, featuredRooms, sortedRooms:rooms, loading:false, maxPrice, maxSize
-        })
-        console.log(rooms);
+    if (capacity !== 1) {
+      tempRooms = tempRooms.filter(room => room.capacity >= capacity);
     }
 
-    formatData(items) {
-        let tempItems = items.map(item => {
-            let id = item.sys.id;
-            let images = item.fields.images.map(image => image.fields.file.url);
-            let room = {...item.fields, images, id}
-            return room;
-        })
-        return tempItems;
+    tempRooms = tempRooms.filter(room => room.price <= price);
+
+    tempRooms = tempRooms.filter(
+      room => room.size >= minSize && room.size <= maxSize
+    );
+
+    if (breakfast) {
+      tempRooms.filter(room => room.breakfast === true);
     }
 
-    getRoom = (slug) => {
-        let tempRooms = [...this.state.rooms];
-        const room = tempRooms.find((room) => room.slug === slug)
-        return room
+    if (pets) {
+      tempRooms.filter(room => room.pets === true);
     }
 
-    handleChange = event => {
-        const target = event.target;
-        const value = target.type === 'checkbox' ? target.checked:target.value;
-        const name = event.target.name;
-        this.setState({
-            [name]:value
-        }, this.filterRooms)
-    }
+    this.setState({
+      sortedRooms: tempRooms
+    });
+  };
 
-    filterRooms = () => {
-        let {rooms, type, capacity, price, minSize, minPrice, maxPrice, maxSize, breakfast, pets} = this.state
-
-        let tempRooms = [...rooms];
-
-        capacity = parseInt(capacity)
-        price = parseInt(price)
-
-        if (type !== 'all') {
-            tempRooms = tempRooms.filter(room => room.type === type)
-        }
-
-        if (capacity !== 1) {
-            tempRooms = tempRooms.filter(room => room.capacity >= capacity)
-        }
-
-        tempRooms = tempRooms.filter(room => room.price <= price)
-
-        tempRooms = tempRooms.filter(room => room.size >= minSize && room.size <= maxSize)
-
-        if (breakfast) {
-            tempRooms.filter(room => room.breakfast === true)
-        }
-
-        if (pets) {
-            tempRooms.filter(room => room.pets === true)
-        }
-
-        this.setState({
-            sortedRooms:tempRooms
-        })
-    }
-
-    
-
-    render() {
-        return <RoomContext.Provider value={{...this.state, getRoom:this.getRoom, handleChange:this.handleChange}}>{this.props.children}</RoomContext.Provider>    
-    }
+  render() {
+    return (
+      <RoomContext.Provider
+        value={{
+          ...this.state,
+          getRoom: this.getRoom,
+          handleChange: this.handleChange
+        }}
+      >
+        {this.props.children}
+      </RoomContext.Provider>
+    );
+  }
 }
 
 const RoomConsumer = RoomContext.Consumer;
